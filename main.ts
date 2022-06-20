c7d4df12fd9cf5dff023f28d84216e5239dedffd
藍+85CD <50108258+kwaa@users.noreply.github.com>
2022-06-20 15:15:58 +0800

    Initial commit

---
+++ main.ts
@@ -0,0 +1,17 @@
+/** @jsx h */
+import blog, { h } from "https://deno.land/x/blog/blog.tsx";
+
+blog({
+  author: "藍+85CD",
+  title: "./kwaa.dev/bk",
+  description: "藍+85CD@がんばらない",
+  avatar: "https://kwaa.dev/assets/maskable@192.webp",
+  avatarClass: "rounded-full",
+  links: [
+    { title: "GitHub", url: "https://github.com/kwaa" },
+  ],
+  background: "#f9f9f9",
+  // header: <header><link rel="shortcut icon" href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAMAAABg3Am1AAAAVFBMVEUAAADW29T///+5wcc4ODjz8OZVVVX/7MkVFRXtrpqTJiVHKxPZOjr/+/QICz3/07d1TCNErbkaSXtgERn59vb//OPSzMzuuwKZne20srL0mIyWGwWygNgKAAAAAXRSTlMAQObYZgAAAT1JREFUSMetkttuhSAQRVvhVMSKCFTP5f//s3uLE+OrzEokI+yVDJevK93BN5D6mmgTGDRAJNYUtQRGpsmYEBir8ZRCoKIhMP54GLMAVhVqougIn09KbKwqbIyKnmAAo1Q41kpXYAsMcvsTkJpKmyCHSokRGfselapAKJEQesBrUxOAbDuE5zMlCrxI6joCl+QrZVn+wLLwX00AsvH+YJ41BImTnH+B98bMO95jsklgPMazIQu8p1K/nHUEeWLW/ux4/357kLOOMI4Mi2Ctc7WhdX29jNERoIDakHOy7VLW1VoobcKpiAD8jrWlOCePu1WI0e44N44Uts17HgBhXEOQhijUWsYOtAoDkIbq5W0bR6iAq1BuC4z3B8MQo7swz7LSgfsC4SQbYkuCtXUeNAhUeLCIA4mfCqMSviH8A0vlJt0MamaSAAAAAElFTkSuQmCC" sizes="24x24" type="image/x-icon"></header>
+  // lang: "ja",
+  // timezone: "zh-TW"
+});
